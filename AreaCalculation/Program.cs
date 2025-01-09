da22d65c4300efd1dcb14546049049ff4c062a63
Baybars <baybarsstanridagli@gmail.com>
2025-01-09 13:15:51 +0300

    AreaCalculation

---
+++ AreaCalculation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int shortEdge, longEdge, area, environment;
+            Console.WriteLine("Please Enter The Short Edge: ");
+            shortEdge= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter The Long Edge: ");
+            longEdge= Convert.ToInt32(Console.ReadLine());
+            area = shortEdge * longEdge;
+            environment = 2 * (shortEdge + longEdge);
+            Console.WriteLine("Area: "+ area);
+            Console.WriteLine("Environment: " + environment);
+            Console.ReadKey();
+
+        }
+    }
+}
